df7e08efbbc319b39d8f85e70fdd3ae872eea2dc
Vincent Sardinas <22344@columbushs.com>
2019-11-12 14:44:02 -0500

    commit

--- mars-rover-v2/javascripts/app.js
+++ mars-rover-v2/javascripts/app.js
@@ -1,15 +1,43 @@
 // Rover Object Goes Here
 // ======================
-
+ rover = {direction: "N", x: 0, y: 0}
 // ======================
 function turnLeft(rover){
+  if (rover.direction =="N"){
+    rover.direction =="W"
+  } else if (rover.direction  == "W"){
+    rover.direction =="S"
+  } else if (rover.direction == "S"){
+    rover.direction =="E";
+  }else if (rover.direction == "E"){
+      rover.direction =="N";
+  }
   console.log("turnLeft was called!");
 }
 
 function turnRight(rover){
+  if (rover.direction ="N"){
+    rover.direction ="E"
+  } else if (rover.direction  = "E"){
+    rover.direction ="S";
+  } else if (rover.direction = "S"){
+    rover.direction ="W";
+  } else if (rover.direction = "W"){
+    rover.direction ="N";
+  }
   console.log("turnRight was called!");
 }
 
 function moveForward(rover){
+  if (rover.direction == "N"){
+    rover.y = y+1;
+  } else if (rover.direction == "W"){
+    rover.x =rover.x -1;
+  } else if (rover.direction == "S"){
+    rover.y = y-1;
+  } else if (rover.direction == "E"){
+    rover.x == rover.x +1;
+  }
   console.log("moveForward was called")
 }
+
